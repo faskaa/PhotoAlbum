087f3f28c079fea3fd1917abed7ddc54b8c6d967
faskaa <fazilnb@code.edu.az>
2023-08-14 16:29:31 +0400

    first commit initizailed

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp2
+{
+  public class PhotoBook
+    {
+        public int numPages { get; set; }
+
+        public PhotoBook()
+        {
+            numPages = 16;
+        }
+        public PhotoBook(int number)
+        {
+            numPages = number;
+        }
+        public int GetNumberPages()
+        {
+            return numPages;
+        }
+
+    }
+    
+   public class BigPhotoBook
+    {
+        public int numPages { get; set; }
+
+        public BigPhotoBook()
+        {
+            numPages = 64;
+        }
+        public BigPhotoBook(int number)
+        {
+            numPages = number;
+        }
+        public int GetNumberPages()
+        {
+            return numPages;
+        }
+    } 
+    
+    
+    
+    
+    
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var album1 = new BigPhotoBook();
+            Console.WriteLine(album1.GetNumberPages());
+            var album2 = new PhotoBook();
+            Console.WriteLine(album2.GetNumberPages());
+            var album3 = new PhotoBook(24);
+            Console.WriteLine(album3.GetNumberPages());
+        }
+    }
+}
